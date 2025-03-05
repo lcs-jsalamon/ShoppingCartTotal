9c7c557e08da2f68bfa1e6ae052a8cccca81361f
Josephine Salamon <jsalamon@lcs.on.ca>
2025-03-05 09:24:26 -0500

    Initial Commit

---
+++ ShoppingCartTotal/ShoppingCartTotalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingCartTotalApp.swift
+//  ShoppingCartTotal
+//
+//  Created by Josephine Salamon on 2025-03-05.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingCartTotalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShoppingCartTotalTests/ShoppingCartTotalTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingCartTotalTests.swift
+//  ShoppingCartTotalTests
+//
+//  Created by Josephine Salamon on 2025-03-05.
+//
+
+import Testing
+@testable import ShoppingCartTotal
+
+struct ShoppingCartTotalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
